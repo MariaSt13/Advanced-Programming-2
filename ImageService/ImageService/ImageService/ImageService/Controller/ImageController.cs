a224ef62c3607736752d622f9b3ce205a7b1d178
Maria Strelecky <mariast13@gmail.com>
2018-04-11 18:11:56 +0300

    new file

--- ImageService/ImageService/ImageService/ImageService/Controller/ImageController.cs
+++ ImageService/ImageService/ImageService/ImageService/Controller/ImageController.cs
@@ -17,15 +17,16 @@ namespace ImageService.Controller
         public ImageController(IImageServiceModal modal)
         {
             // Storing the Modal Of The System
-            m_modal = modal;                    
+            m_modal = modal;
             commands = new Dictionary<int, ICommand>()
             {
-                // For Now will contain NEW_FILE_COMMAND
+                { 1, new NewFileCommand(m_modal) }
             };
         }
         public string ExecuteCommand(int commandID, string[] args, out bool resultSuccesful)
         {
-            // Write Code Here
+            ICommand command = this.commands[commandID];
+            return command.Execute(args, out resultSuccesful);
         }
     }
 }
