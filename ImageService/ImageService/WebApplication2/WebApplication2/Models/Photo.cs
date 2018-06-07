b7f3c7dd0b534c4ed2c72810a971f744b02ff457
linoy <linoya995@gmail.com>
2018-06-08 00:29:16 +0300

    ////

---
+++ ImageService/ImageService/WebApplication2/WebApplication2/Models/Photo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Photo
+    {
+        public Photo(string path, string name, string date)
+        {
+            this.Path = path;
+            this.Name = name;
+            this.Date = date;
+        }
+        [DataType(DataType.Text)]
+        [Display(Name = "Path")]
+        public string Path { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name = "Date")]
+        public string Date { get; set; }
+    }
+}
--- ImageService/ImageService/WebApplication2/WebApplication2/Models/PhotosModel.cs
+++ ImageService/ImageService/WebApplication2/WebApplication2/Models/PhotosModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,22 @@ namespace WebApplication2.Models
 {
     public class PhotosModel
     {
+        private List<Photo> _photos;
+    }
+
+    private List<Photo> readPhotosInfo()
+    {
+        string path = HttpContext.Current.Server.MapPath("../PhotosOutput");
+        string[] photosPaths = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+
+        List<Photo> list = new List<Photo>();
+        string[] lines = File.ReadAllLines(HttpContext.Current.Server.MapPath("/App_Data/student_info.txt"));
+        for (int i = 0; i < photosPaths.Length; i++)
+        {
+            string photoName = Path.GetFileName(photosPaths[i]);
+            string photoPath = photosPaths[i];
+        }
+        return list;
     }
 }
