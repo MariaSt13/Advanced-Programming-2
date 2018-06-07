3fe5372cbc10bb702f768176fc997bfb2cc02a59
linoy <linoya995@gmail.com>
2018-06-08 00:40:34 +0300

    .

--- ImageService/ImageService/WebApplication2/WebApplication2/Models/PhotosModel.cs
+++ ImageService/ImageService/WebApplication2/WebApplication2/Models/PhotosModel.cs
@@ -9,21 +9,33 @@ namespace WebApplication2.Models
     public class PhotosModel
     {
         private List<Photo> _photos;
-    }
 
-    private List<Photo> readPhotosInfo()
-    {
-        string path = HttpContext.Current.Server.MapPath("../PhotosOutput");
-        string[] photosPaths = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
-
-
-        List<Photo> list = new List<Photo>();
-        string[] lines = File.ReadAllLines(HttpContext.Current.Server.MapPath("/App_Data/student_info.txt"));
-        for (int i = 0; i < photosPaths.Length; i++)
+        public PhotosModel()
         {
-            string photoName = Path.GetFileName(photosPaths[i]);
-            string photoPath = photosPaths[i];
+            this._photos = readPhotosInfo();
         }
-        return list;
+
+        private List<Photo> readPhotosInfo()
+        {
+            string path = HttpContext.Current.Server.MapPath("../PhotosOutput");
+            string[] photosPaths = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+
+            List<Photo> list = new List<Photo>();
+            string[] lines = File.ReadAllLines(HttpContext.Current.Server.MapPath("/App_Data/student_info.txt"));
+            for (int i = 0; i < photosPaths.Length; i++)
+            {
+                string photoName = Path.GetFileName(photosPaths[i]);
+                string photoPath = photosPaths[i];
+                string month = Path.GetFileName(Path.GetDirectoryName(photoPath));
+                string year = Path.GetFileName(Path.GetDirectoryName(Path.GetDirectoryName(photoPath)));
+                string photoDate = "Year:" + year + "\n" + "Month" + month;
+                Photo photo = new Photo(photoPath, photoName, photoDate);
+                list.Add(photo);
+            }
+            return list;
+        }
     }
+
+  
 }
